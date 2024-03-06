925c7a368a2d0335080ee4c4dad88ff5024bfdf4
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-03-06 14:26:39 -0500

    Create transaction view

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.codercampus.Assignment11.domain.Transaction;
 import com.codercampus.Assignment11.service.TransactionService;
@@ -26,6 +26,13 @@ public class TransactionController {
 		model.put("transactions", allTransactions);
 
 		return "transactions";
+	}
+	
+	@GetMapping("/transactions/{transactionId}")
+	public String getTransaction (@PathVariable int id, ModelMap model) {
+		Transaction transaction = transactionService.findById(id);
+		model.put("transaction", transaction);
+		return "transaction-view";
 	}
 
 }
