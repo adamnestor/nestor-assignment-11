02c61c8aa8b38e034ad2f6ead3b3184f4de80a74
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-03-06 09:33:17 -0500

    creating /transactions functionality

--- src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -1,5 +1,7 @@
 package com.codercampus.Assignment11.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class TransactionService {
 
 	public Transaction findById(int id) {
 		return transactionRepo.findById(id);
+	}
+
+	public List<Transaction> findAll() {
+		return transactionRepo.findAll();
 	}
 }
