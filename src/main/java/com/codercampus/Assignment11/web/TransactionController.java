d9b25615df7ee8630734a51fd16b3ca8bef7fc1f
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-03-06 07:20:06 -0500

    Viewing assignment formatting

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,6 +1,15 @@
 package com.codercampus.Assignment11.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TransactionController {
 
+	@GetMapping("/transactions")
+	public String getTransactions(ModelMap model) {
+		return "transactions";
+	}
 	
 }
